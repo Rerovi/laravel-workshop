ca4db9c7e6d54ce766724228ce7f062e96ad72fd
Rerovi <rerovi-rev@outlook.com>
2022-02-10 16:04:37 +0100

    Opdracht 2
    Projects, Categories tabellen geseed met faker en factories.

--- example-app/database/factories/ProjectFactory.php
+++ example-app/database/factories/ProjectFactory.php
@@ -2,24 +2,26 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class CategoryFactory extends Factory
+
+class ProjectFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    protected $model = Category::class;
-
+    protected $model = Project::class;
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' =>$this->faker->sentence($nbWords = 2, $variableNbWords = true),
+            'description' =>$this->faker->sentence($nbWords = 6, $variableNbWords = true),
             'created_at' => $this->faker->dateTimeThisDecade('now', 'europe/amsterdam'),
             'updated_at' => $this->faker->dateTimeThisDecade('now', 'europe/amsterdam')
+
         ];
     }
 }
